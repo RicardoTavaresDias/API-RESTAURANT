dee052580404dea3c70de0951e63dbc6ef28ecf3
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-01-23 14:42:09 -0300

    Projeto finalizado API de restaurante

---
+++ src/routes/tables-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { TablesController } from "@/controllers/tables-controller";
+
+const tablesRoutes = Router()
+const tableController = new TablesController()
+
+tablesRoutes.get("/", tableController.index)
+
+export { tablesRoutes }
